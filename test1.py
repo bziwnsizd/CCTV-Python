e802c6abb1d081123f8770c1cf3174872ba43113
bziwnsizd <69657523+bziwnsizd@users.noreply.github.com>
2020-08-19 13:39:28 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ import folium
 import folium.plugins
 import math
 import gpxpy.geo
-from decimal import *
+
 #강의실
 a = pd.read_csv('C:/Users/USER/Desktop/SK infosec/SK-Infosec/tens/crime.csv', thousands=',',encoding='euc-kr')
 b = pd.read_csv('C:/Users/USER/Desktop/SK infosec/SK-Infosec/tens/cctv1.csv', thousands=',',encoding='euc-kr')
@@ -68,23 +68,62 @@ def search_map(search_text):
         return response_body.decode('utf-8')
     else:
         print("Error Code:" + rescode)
-# 사용자 입력값을 api로 위/경도 변환 함수 + 폴리곤화(색상 블루, 크기 cctv보다 작게)
-def getData(parameter):
-    request = urllib.request.Request(parameter)
-    response = urllib.request.urlopen(request)
-    rescode = response.getcode()
-
-    response_body = response.read().decode('utf-8')
-    result = response_bdy
-    temp_user = search_map(result)
+# html button click
+def crrosover(parameter):
+    
+    temp_user = search_map(parameter)
     temp_user = json.load(temp_user)
     temp_user = temp_user['addresses'][0]
 
-    user_info = (float(temp_user['x']),float(temp_user['y']))
-    #사용자 위/경도 (수정예정)
-    return user_info
+    R2_p = (float(temp_user['x']),float(temp_user['y']))
+    offsets = calc_offsets(radi_user, R2_p[1])
+    coordinates = [coordinate_after_rotation(R2_p, e, offsets) for e in range(0, 360+1, rotating_degree)]
+    asz = []
+    for i in coordinates:
+        asz.append(i)
 
-#수정예정 R2_p -> 함수에서 나온 사용자 위도/경도
+    folium.Marker(
+        R2_p,
+        icon = folium.Icon(color='blue'), #icon='C:/Users/His hacker/Desktop/gitlab/SK-Infosec/tens/images.png'
+        popup = R2_p
+    ).add_to(fg_2)
+    #각 cctv의 폴리곤 원형화
+    folium.Polygon(
+        locations = asz,
+        fill = True,
+        color = 'blue',
+        tooltip = 'Polygon'
+    ).add_to(fg_2)
+    
+    c = cc[i],tv[i]
+    d = distance(c, R2_p)
+    tmp_array = [cc[i], tv[i], d]
+    lst.append(tmp_array)
+    crros_over = min(lst, key=lambda item: item[2])
+    R1_p = (crros_over[0], crros_over[1])
+    pulse = (crros_over[2])
+
+       #0.013  10   50
+    if pulse + R <= r : # 장소의 원안에 유저의 원이 존재할 경우
+        ##error ar 값이 r과 R로만 이루어짐
+        ar = (math.pi * pow(R, 2)) / (math.pi * pow(r, 2))
+        print("첫번째")
+    elif pulse + r <= R : #유저의 원안에 장소의 원이 존재할 경우
+        ar = 1.0
+        print("두번째")
+    elif pulse < R + r : #두 원이 겹쳐지지 않은 경우 + 부분적으로 겹쳐진경우
+        ar = intersection_area(R, r, d) / (math.pi * pow(r, 2))
+        print("세번째")
+    else :
+        print("에러")
+    print(ar)
+
+#crrorover() 사용자 GPS 입력값
+#######################################
+#if parameter가 있을경우 아래 실행      #
+#crrosover(parameter)                 #
+#######################################
+
 
 #csv에서 지역을 받아 리스트로 넣어줌
 region = []
@@ -99,9 +138,19 @@ y = [] #네이버 api에서 받은 경도
 z = [] #네이버 api에서 받은 지역이름
 cc = [] #cctv 위도
 tv = [] #cctv 경도
-
+lst = []
 #web-site user input 
+#사용자가 입력할때
+#홈페이지에서 버튼 클릭시 수행
 
+#    request = urllib.request.Request('http://localhost:8000') # web-site input gps
+#    response = urllib.request.urlopen(request)
+#    rescode = response.getcode()
+
+#    response_body = response.read().decode('utf-8')
+#    result = response_body
+#    if result == not null :
+#       getData(result)
 
 #naver map api info
 for value in region:
@@ -144,26 +193,19 @@ for i in range(373):
     cc.append(b.iloc[i, 2]) #위도
     tv.append(b.iloc[i, 3]) #경도
 
-
 radi = 30 #반경
 radi_user = 15 #유저반경
 rotating_degree = 10 #회전각도 5개
 ar = 0 #비율, 교차하지 않은 경우의 값
 r = 50 #장소의 원의 반지름
 R = 10 #유저의 원의 반지름
-R2_p = (37.459590, 126.904861) ##수정예정 R2_p -> 함수에서 나온 사용자 위도/경도
-lst = []
+
 #cctv input
 for i in range(373):
     # c = 각 cctv의 좌표
     c = cc[i],tv[i] #36.94273371 / 126.780918
     #사용자 입력 주소 (위,경도 변환된값)
-    d = distance(c, R2_p)
     # tmp_dict = {"lat": cc[i], "lot": tv[i], "distance": d}
-    tmp_array = [cc[i], tv[i], d]
-    #d = math.floor(d)
-    # lst.append(d)
-    lst.append(tmp_array)
     offsets = calc_offsets(radi, c[1])
     coordinates = [coordinate_after_rotation(c, e, offsets) for e in range(0, 360+1, rotating_degree)]
     # b = 각 cctv의 폴리곤화
@@ -184,57 +226,18 @@ for i in range(373):
     tooltip = 'Polygon'
     ).add_to(fg_2)
 
-search = min(lst, key=lambda item: item[2])
-print(search)
+
 
 # 교차 면적 함수 구하기
 # cctv = 37.4596667, 126.90474640000002
 # R2_p = 37.459485, 126.905046
 # distance = 0.03299627717084893 
-R1_p = (search[0], search[1])
-pulse = (search[2])
-   #0.013  10   50
-if pulse + R <= r : # 장소의 원안에 유저의 원이 존재할 경우
-    ##error ar 값이 r과 R로만 이루어짐
-    ar = (math.pi * pow(R, 2)) / (math.pi * pow(r, 2))
-    print("첫번째")
-elif pulse + r <= R : #유저의 원안에 장소의 원이 존재할 경우
-    ar = 1.0
-    print("두번째")
-elif pulse < R + r : #두 원이 겹쳐지지 않은 경우 + 부분적으로 겹쳐진경우
-    ar = intersection_area(R, r, d) / (math.pi * pow(r, 2))
-    print("세번째")
-else :
-    print("에러")
-print(ar)
+
 
 folium.LayerControl(collapsed=False).add_to(m)
-
-# coordinates = [coordinate_after_rotation(c, e, offsets) for e in range(0, 360+1, rotating_degree)]
-# # b = 각 cctv의 폴리곤화
-# b = []
-# for i in coordinates:
-#     b.append(i)
-offsets = calc_offsets(radi_user, R2_p[1])
-coordinates = [coordinate_after_rotation(R2_p, e, offsets) for e in range(0, 360+1, rotating_degree)]
-asz = []
-for i in coordinates:
-    asz.append(i)
-#수정예정 R2_p -> 함수에서 나온 사용자 위도/경도
-folium.Marker(
-    R2_p,
-    icon = folium.Icon(color='blue'), #icon='C:/Users/His hacker/Desktop/gitlab/SK-Infosec/tens/images.png'
-    popup = R2_p
-).add_to(fg_2)
-
-#수정예정 R2_p -> 함수에서 나온 사용자 위도/경도
-#각 cctv의 폴리곤 원형화
-folium.Polygon(
-    locations = asz,
-    fill = True,
-    color = 'blue',
-    tooltip = 'Polygon'
-).add_to(fg_2)
-
 m.save('complete.html')
 
+
+#경찰서 좌표 찍기
+#
+#cctv 이미지, 경찰서 좌표 및 이미지
